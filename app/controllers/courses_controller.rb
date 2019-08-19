a0a39f6a81942258ec5fe7a47e8ce0c9c687c0c3
Salvatore Santamaria <sam.santamaria@gmail.com>
2019-08-19 08:55:02 -0700

    Added db validations and output to index.html

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,4 +1,7 @@
 class CoursesController < ApplicationController #courses controller inheriting from app controller
     def index #directs to app/views/courses/index.html.erb
+        @courses = Course.all #this make this available to all of the views
+    end
+    def new
     end
 end
