802b72df9539f2bbf89cb2be7bddc3cb35bd6df8
Salvatore Santamaria <sam.santamaria@gmail.com>
2019-08-28 09:41:30 -0700

    Added login/logout function

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -4,9 +4,21 @@ class LoginsController < ApplicationController
     end
 
     def create
+        student = Student.find_by(email: params[:logins][:email].downcase) #grab the student
+        if student && student.authenticate(params[:logins][:password]) #ifstudent exists
+            session[:student_id] = student.id   #set session variable
+            flash[:notice] = "You have successfully loggen in"
+            redirect_to student
+        else  
+            flash.now[:notice] = "Something was wrong with you login information" #.now will show just on page and then leave, will not store for redirect
+            render 'new'
+        end
     end
 
     def destroy
+        session[:student_id] = nil
+        flash[:notice] = "You have successfully logged out"
+        redirect_to root_path
     end
 
 end
