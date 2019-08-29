7ccf648e9dc2a401ba2faa48b53fcad1d0d16331
Salvatore Santamaria <sam.santamaria@gmail.com>
2019-08-29 08:42:41 -0700

    Added logged safes

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,4 +1,7 @@
 class CoursesController < ApplicationController #courses controller inheriting from app controller
+    
+    skip_before_action :require_user
+    
     def index #directs to app/views/courses/index.html.erb
         @courses = Course.all #this make this available to all of the views
     end
